1524cae74415dffa3f32d7c10aafcf4d07960914
chulsoo <chulsoo@gmail.com>
2021-09-06 10:00:00 +0000

    modify my_compare

--- my_compare.cc
+++ my_compare.cc
@@ -1,17 +1,23 @@
 #include "my_compare.hh"
+#include <iostream>
 
-char is_equal(int a, int b){
-    if(a==b)    
-        return 'O';
-    else
-        return 'X';
+char isDuplicate(int biggest, int* num_arr){
+    /* TODO: Your code goes here and fix existing code */   
+    for(int i=0; i<3; i++){
+        if(biggest = num_arr[i])
+            return 'O';
+    }
+
+    return 'X';
 }
 
 
-char is_big(int a, int b){
-    
-    if(a>b)
-        return 'O';
-    else   
-        return 'X';
+int getBiggestNum(int* num_arr){
+    /* TODO: Your code goes here and fix existing code */   
+    if(num_arr[0] > num_arr[1] > num_arr[2])
+        return num_arr[0];
+    else if(num_arr[1] > num_arr[2] > num_arr[1])
+        return num_arr[1];
+    else if(num_arr[2] > num_arr[1] > num_arr[0])
+        return num_arr[2];
 }
--- my_compare.hh
+++ my_compare.hh
@@ -1,3 +1,18 @@
 
-char is_equal(int a, int b); //'O' if a==b else 'X'
-char is_big(int a, int b); //'O' if a>b else 'X'
+
+/**
+ * @brief:
+ *
+ * @return: (int) the biggest number
+ */
+int getBiggestNum(int* num_arr);
+
+
+/**
+ * @brief: check for duplicate number 
+ *         with the biggest number in array
+ *
+ * @return: (char) 'O' : exist.
+ *          (char) 'X' : not exits.
+ */
+char isDuplicate(int biggest, int* num_arr);
