dca925434180547d8668211e7a199ed0e10aa947
chulsoo <chulsoo@gmail.com>
2021-09-06 10:00:00 +0000

    modify main.cc

--- main.cc
+++ main.cc
@@ -1,24 +1,31 @@
 #include <iostream> //I/O Header File
-#include "my_math.hh"
 #include "my_compare.hh"
 
 using namespace std;
 
-// The C++ compiler will look for a function
-// called “main”
+
+// Insert yout student ID
+#define STUDENT_ID 2000000
+
+
+/**
+ *
+ * Warning: Don't modify code below !!
+ *
+ */
 int main(int argc, char* argv[]) {
-    //My Math Function
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    int add_res = my_add(a, b);
-    int mul_res = my_mul(a, b);
-    char add_check = is_equal(add_res, a+b);
-    char mul_check = is_equal(mul_res, a*b);
-    char compare = is_big(a, b);
 
-    cout << "[Student ID] : " << add_check << " " << mul_check << " " << compare << endl;
+    int num_arr[3];
+    
+    for(int i=0; i<argc-1; i++)
+        num_arr[i] = atoi(argv[i+1]);
 
-    return 0;  // must return an int to indicate
-                // successful program completion
+    int biggest_num = getBiggestNum(num_arr);
+    char compare = isDuplicate(biggest_num, num_arr);
+
+    cout << STUDENT_ID << ": " << "The Biggest Number is " << biggest_num  \
+         << " and Duplicate number existence " << compare << endl;
+
+    return 0;
 }
 
