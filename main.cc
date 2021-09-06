76a2b4ff0ff8345fd7bcbd96dccc42e8826ac33e
youngsoo <younsoo@gmail.com>
2021-09-06 18:00:00 +0000

    modify main.cc

--- main.cc
+++ main.cc
@@ -23,8 +23,7 @@ int main(int argc, char* argv[]) {
     int biggest_num = getBiggestNum(num_arr);
     char compare = isDuplicate(biggest_num, num_arr);
 
-    cout << STUDENT_ID << ": " << "The Biggest Number is " << biggest_num  \
-         << " and Duplicate number existence " << compare << endl;
+    cout << STUDENT_ID << ": "<< biggest_num << " "<< compare << endl;
 
     return 0;
 }
